9a145be7628a42bdd7bc9c257a1a3b7522ecb83c
YUVRAJ06singh08deora <06yuvraj2001singh@gmail.com>
2022-04-19 20:45:37 +0530

    Initial commit

---
+++ app/src/main/java/com/example/krishitech/splashActivity.java
@@ -0,0 +1,33 @@
+package com.example.krishitech;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class splashActivity extends AppCompatActivity {
+TextView appname;
+LottieAnimationView lottie;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        appname=findViewById(R.id.kritech);
+        lottie=findViewById(R.id.lottie);
+        appname.animate().translationY(-1400).setDuration(2700).setStartDelay(0);
+        lottie .animate().translationX(2000).setDuration(2000).setStartDelay(2900);
+         new Handler().postDelayed(new Runnable() {
+             @Override
+             public void run() {
+           Intent i=new Intent(getApplicationContext(),MainActivity.class);
+           startActivity(i);
+             }
+         },5000);
+
+    }
+}
